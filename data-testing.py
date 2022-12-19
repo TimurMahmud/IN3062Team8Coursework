1e7a8251d5ae7a5a5c45ebb05ea63286ed22c684
Timur Mahmud <71776558+TimurMahmud@users.noreply.github.com>
2022-12-19 17:44:07 +0000

    data testing Timur

---
+++ data-testing.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 4 17:31:26 2022
+
+@author: timur
+"""
+
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+df = pd.read_csv("winequality-red.csv")
+
+le = LabelEncoder()
+le.fit(df["quality"].unique())
+df["quality"] = le.transform(df["quality"])
+
+# Shows the features and target with some data
+print(df.head())
+
+# Shows correlation between features and target
+print(df.corr()['quality'].sort_values(ascending=False)[1:])
+      
+# Shows columns
+print(df.columns)
+
+# Checks for null data inputs
+print(df.isnull().sum().sum())
+
+# Shows Non-null count and data types of each column
+print(df.info())
+
+# Transpose index and columns
+print(df.describe().T)
+
+
+
